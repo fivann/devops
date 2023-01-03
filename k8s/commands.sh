a0cfb651a2b0d522d7c3cdef444dc66b90c9ee8a
fivann <fyodorov.i.n@gmail.com>
2023-01-03 18:11:46 +0400

    Update commands.sh

--- k8s/commands.sh
+++ k8s/commands.sh
@@ -1,11 +1,15 @@
 ### some useful commands
 # pods
 kubectl get po -l tier=frontend
+kubectl get po -o wide
 # services
 kubectl get svc -l tier=frontend
-kubectl get svc -l app=nginx-service --all-namespaces
-
+kubectl get svc -l app=nginx-app --all-namespaces
+kubectl get svc -l app=nginx-app
+kubectl delete svc nginx-service
+kubectl describe svc -l app=nginx-app
 # nodes
 kubectl get no -o wide
+kubectl describe no node01 
 #
 kubectl delete -f myapp.yaml
