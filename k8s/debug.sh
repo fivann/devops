d34e2a28a42bdd3195c9bbff3320080725c4e6ed
fivann <fyodorov.i.n@gmail.com>
2023-01-04 15:42:07 +0400

    Update debug.sh

--- k8s/debug.sh
+++ k8s/debug.sh
@@ -9,3 +9,5 @@ kubectl logs nginx-ingress-controller-66f656b54b-h8b4l
 # ✖ The cluster seems to be running with a restrictive Authorization mode and the Ingress controller does not have the required permissions to operate normally.
 # It looks like the cluster is running with a restrictive authorization mode and the Ingress controller does not have the necessary permissions to operate correctly. To fix this issue, you will need to give the Ingress controller the necessary permissions to operate.
 # One way to do this is to use a ClusterRole and ClusterRoleBinding to grant the Ingress controller the necessary permissions.
+
+kubectl exec nginx-ingress-controller-65886f4f5d-xgdxs -n ingress-nginx -it -- cat /etc/nginx/nginx.conf | nl | grep access_logymc
