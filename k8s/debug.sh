546b843a5cbfaa721a85c8866813df6fe7a17672
fivann <fyodorov.i.n@gmail.com>
2023-01-04 12:24:59 +0400

    Create debug.sh

---
+++ k8s/debug.sh
@@ -0,0 +1,10 @@
+# pod is in CrashLoopBackOff status, how to debug
+kubectl get po --all-namespaces
+# smth like
+# default         nginx-ingress-controller-66f656b54b-h8b4l   0/1     CrashLoopBackOff   4          2m32s
+kubectl logs [pod-name]
+kubectl logs nginx-ingress-controller-66f656b54b-h8b4l
+# smth like 
+# ✖ The cluster seems to be running with a restrictive Authorization mode and the Ingress controller does not have the required permissions to operate normally.
+# It looks like the cluster is running with a restrictive authorization mode and the Ingress controller does not have the necessary permissions to operate correctly. To fix this issue, you will need to give the Ingress controller the necessary permissions to operate.
+# One way to do this is to use a ClusterRole and ClusterRoleBinding to grant the Ingress controller the necessary permissions.
