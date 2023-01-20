f10e43cbffc084cfce55946ac8d102d3dfbabaab
fivann <fyodorov.i.n@gmail.com>
2023-01-20 20:55:11 +0400

    Update checkingRunningProcessStatusAndKillItByTimeout.sh

--- sh/checkingRunningProcessStatusAndKillItByTimeout.sh
+++ sh/checkingRunningProcessStatusAndKillItByTimeout.sh
@@ -32,3 +32,40 @@ done
 # it will display the message "done!" and exit with a zero exit code. 
 # If the status is "running", the script will check if the command has been running for more than 30 minutes (1800 seconds) and if so, 
 # it will display the message "timeout, terminating" and kill the "checkbackups" process using the pkill command.
+
+
+###################################################################################################
+## the same but using mail
+###################################################################################################
+
+
+#!/bin/bash
+
+timeout=1800 # 30 minutes in seconds
+start_time=$(date +%s)
+admin_email="admin@myproject.net"
+
+# Run the checkbackups command
+checkbackups &
+
+while true; do
+    status=$(checkbackups -s)
+
+    if [ "$status" == "failed" ]; then
+        echo "failed!" | mail -s "Checkbackups status: Failed" $admin_email
+        exit 1
+    elif [ "$status" == "done" ]; then
+        echo "done!"
+        exit 0
+    elif [ "$status" == "running" ]; then
+        current_time=$(date +%s)
+        if [ $((current_time - start_time)) -gt $timeout ]; then
+            echo "timeout, terminating" | mail -s "Checkbackups status: Timeout" $admin_email
+            pkill checkbackups
+            exit 1
+        fi
+    fi
+
+    sleep 1
+done
+
