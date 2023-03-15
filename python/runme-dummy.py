11ee76484478d5b767dc3ed8317172cb2e3df157
fivann <fyodorov.i.n@gmail.com>
2023-03-15 14:48:57 +0400

    Update runme-dummy.py

--- python/runme-dummy.py
+++ python/runme-dummy.py
@@ -1,9 +1,19 @@
 import datetime
 import time
+import os
+import sys
+
+# Dummy runmy script to pring timestamp once in awhile
+
+log_file_path = "/log/timestamp.log"
 
 while True:
     current_time = datetime.datetime.utcnow()
     current_time_formatted = current_time.strftime("%Y-%m-%d %H:%M:%S")
-    with open("/log/timestamp.log", "a") as log_file:
-        log_file.write(current_time_formatted + "\n")
+    try:
+        with open(log_file_path, "a") as log_file:
+            log_file.write(current_time_formatted + " UTC\n")
+    except IOError:
+        print(f"Error: Unable to access file {log_file_path}")
+        sys.exit(1)
     time.sleep(60)
